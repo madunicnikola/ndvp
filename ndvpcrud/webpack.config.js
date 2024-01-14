1dc7396c88d7a1e7fe7cb2c743402e6c28eefb34
Nikola Madunić <madunicnikola@protonmail.com>
2024-01-14 14:02:01 +0100

    project restructuring

--- ndvpcrud/webpack.config.js
+++ ndvpcrud/webpack.config.js
@@ -1,9 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require('webpack');
-
-const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -13,12 +10,10 @@ const stylesHandler = isProduction
 
 const config = {
   entry: {
-    app: './src/index.js',
+    app: './app/page.tsx',
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: '[name].bundle.js',
-    publicPath: ASSET_PATH,
   },
   devServer: {
     open: true,
@@ -31,9 +26,6 @@ const config = {
       title: 'NDVP',
       hash: true,
       filename: 'index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
     }),
   ],
   module: {
